debed3fd9bd60f1579a517f5648dbc0e8cef630a
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-08 22:29:09 +0300

    finishing. clear form after submitting

--- js/main.js
+++ js/main.js
@@ -2,95 +2,116 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // Save Bookmark
-function saveBookmark(e) {
-    // Get form values
-    let siteName = document.getElementById('siteName').value;
-    let siteUrl = document.getElementById('siteUrl').value;
+function saveBookmark(e){
+  // Get form values
+  var siteName =document.getElementById('siteName').value;
+  var siteUrl =document.getElementById('siteUrl').value;
 
-    
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
 
-    let bookmark = {
-        name: siteName,
-        url: siteUrl
-    }
-    console.log(bookmark)
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+  /*
     // Local Storage Test
-    // localStorage.setItem('test', 'Hello World')
-    // console.log(localStorage.getItem('test'))
-    // localStorage.removeItem('test')
-    // console.log(localStorage.getItem('test'))
+    localStorage.setItem('test', 'Hello World');
+    console.log(localStorage.getItem('test'));
+    localStorage.removeItem('test');
+    console.log(localStorage.getItem('test'));
+  */
 
-    if(localStorage.getItem('bookmarks') === null) {
-        // Init array
-        let bookmarks = [];
-        // Add to array
-        bookmarks.push(bookmark);
-        // Set to LocalStorage
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
-    } else {
-        // Get bookmarks from local storage
-        let bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
-        // Add bookmark to array
-        bookmarks.push(bookmark)
-        // Re-set back to Local Storage
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
-    }
-    
-    e.preventDefault();
+  // Test if bookmarks is null
+  if(localStorage.getItem('bookmarks') === null){
+    // Init array
+    var bookmarks = [];
+    // Add to array
+    bookmarks.push(bookmark);
+    // Set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Add bookmark to array
+    bookmarks.push(bookmark);
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
+  // Clear form
+  document.getElementById('myForm').reset();
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
+
+  // Prevent form from submitting
+  e.preventDefault();
 }
 
 // Delete bookmark
 function deleteBookmark(url){
-    // Get bookmarks from localStorage
-    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    // Loop through the bookmarks
-    for(var i =0;i < bookmarks.length;i++){
-      if(bookmarks[i].url == url){
-        // Remove from array
-        bookmarks.splice(i, 1);
-      }
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Loop through the bookmarks
+  for(var i =0;i < bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+      // Remove from array
+      bookmarks.splice(i, 1);
     }
-    // Re-set back to localStorage
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-  
-    // Re-fetch bookmarks
-    fetchBookmarks();
   }
+  // Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
-// Fecth bookmarks
-function fetchBookmarks() {
-    // Get bookmarks from local storage
-    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
-
-    // Get output id
-    let bookmarksResults = document.getElementById('bookmarksResults')
-
-    // Build output
-    bookmarksResults.innerHTML = '';
-    for(let i = 0; i < bookmarks.length; i++) {
-        let name = bookmarks[i].name;
-        let url = bookmarks[i]. url;
-
-        bookmarksResults.innerHTML += '<div class="well">'+
-                                    '<h3 class="name">'+name+
-                                    ' <a class="btn btn-secondary" style="margin-left: 30px" target="_blank" href="'+url+'">Visit</a> '+
-                                    ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" style="margin-left: 10px" href="#">Delete</a> '+
-                                    '</h3>'+
-                                    '</div>';
-    }   
+  // Re-fetch bookmarks
+  fetchBookmarks();
 }
 
-function validateForm() {
-    if(!siteName || !siteUrl) {
-        alert('Please fill in the form')
-        return false;
-    }
+// Fetch bookmarks
+function fetchBookmarks(){
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  // Get output id
+  var bookmarksResults = document.getElementById('bookmarksResults');
 
-    let expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-    let regex = new RegExp(expression);
+  // Build output
+  bookmarksResults.innerHTML = '';
+  for(var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
 
-    if(!siteUrl.match(regex)) {
-        alert('Please use a valid url')
-    };
-    return true;
+    bookmarksResults.innerHTML += '<div class="well">'+
+                                  '<h3>'+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                  '</h3>'+
+                                  '</div>';
+  }
+}
+
+// Validate Form
+function validateForm(siteName, siteUrl){
+  if(!siteName || !siteUrl){
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid URL');
+    return false;
+  }
+
+  return true;
+}
+
+function addhttp(url) {
+  if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+      url = "http://" + url;
+  }
+  return url;
 }
